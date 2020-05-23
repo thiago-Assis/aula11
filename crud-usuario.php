f86908754bb27c3496e56215603f3382a43c33bb
thiago-Assis <65728831+thiago-Assis@users.noreply.github.com>
2020-05-23 13:34:41 -0300

    Sistema CRUD com SESSION

--- crud-usuario.php
+++ crud-usuario.php
@@ -1,7 +1,22 @@
 <?php
-
-     setcookie('usuario','01', time() + (2 *3600));
      
+    ini_set('session.gc_maxlifetime', 180);
+
+
+      session_set_cookie_params(180);
+
+      session_start(); 
+
+      $now = time();
+if (isset($_SESSION['discard_after']) && $now > $_SESSION['discard_after']) {
+    session_unset();
+    session_destroy();
+    session_start();
+}
+
+$_SESSION['discard_after'] = $now + 180;
+
+
        function buscaUsuario($conexao,$email,$senha)
        {
        $senhaMd5 = md5($senha);
